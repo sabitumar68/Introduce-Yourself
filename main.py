2a4fb8900e9b0b078a07d07badd860f4f89972a6
sabitumar68 <sabitumar68@gmail.com>
2024-10-24 11:45:59 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+print("Program 1:Basics of function")
+def intro(name):
+    print("Hello, Good morning! I am", name)
+user_name =input("Enter you name:")
+intro(user_name)
+
+print("Program 2:Factorial")
+
+def recur_factorial(n):
+    if n == 1:
+       return n
+    else:
+        return n*recur_factorial(n-1)
+num = int(input("Enter a number"))
+# check if the number is negative
+if num < 0:
+  print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+ print("The factorial of 0 is 1")
+else:
+ print("The factorial of", num, "is", recur_factorial(num))
+
+ print("Program 3:Calculator")
+ 
+# Program make a simple calculator
+# This function adds two numbers
+def add(x, y):
+  return x + y
+# This function subtracts two numbers
+def subtract(x, y):
+ return x - y
+# This function multiplies two numbers
+def multiply(x, y):
+ return x * y
+# This function divides two numbers
+def divide(x, y):
+ return x / y
+num1 = int(input("Enter Number 1 : "))
+num2 = int(input("Enter Number 2 : "))
+print("Sum :", add(num1, num2))
+print("Difference :", subtract(num1, num2))
+print("Product :", multiply(num1, num2))
+print("Quotient :", divide(num1, num2))
